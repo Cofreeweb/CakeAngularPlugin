5a097685321f676347bc70d4e2b84f462d2ca5f9
Alfonso Etxeberria <alfonso@Arambi.local>
2014-03-25 10:17:41 +0400

    varios

---
+++ Controller/Component/AngularComponent.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * AngularComponent
+ * 
+ *
+ * @package angular.controller.component
+ **/
+
+class AngularComponent extends Component 
+{
+
+
+  public $components = array();
+
+
+
+  public function beforeRender( Controller $Controller) 
+  {
+    if( isset( $Controller->request->params ['ext']) && $Controller->request->params ['ext'] == 'json' && !empty( $Controller->request->data))
+    {
+      if( !array_key_exists( '_serialize', $Controller->viewVars))
+      {
+        $Controller->set( 'data', $Controller->request->data);
+        $Controller->set( '_serialize', array_keys( $Controller->viewVars));
+      }
+      
+    }
+  }
+
+  
+}
+?>
