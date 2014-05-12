f3186952a4dad41ed6f9352322ec075481dc3e29
Alfonso Etxeberria <alfonso@Arambi.local>
2014-05-12 19:30:57 +0400

    a

--- webroot/components/angular-multilingue.js
+++ webroot/components/angular-multilingue.js
@@ -1,13 +1,18 @@
 angular.module( 'multilingue', [])
   .directive( 'multilingue', function( $timeout, $location) {
-    var ret = 'asdfs';
     return {
-        restrict: 'A',
-        replace: true,
-        transclude: false,
-        template: function( tElement, tAttrs) {
-          return "element";
+        restrict: 'E',
+        link: function( scope, element, attrs){
+          scope.$watch( attrs.model, function( newValue, oldValue){
+            if( newValue) {
+              console.log( newValue);
+              for (var i=0; i < newValue.length; i++) {
+                newValue[i]
+              };
+              
+              return $(element).html( 'leches')
+            }
+          })
         }
-
     }
   });
